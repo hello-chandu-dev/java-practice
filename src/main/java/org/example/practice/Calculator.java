178be51d499fffef6cdc56d78453e7e6e19302fb
Praveen Kamalapuram <praveenkamalapuram@Praveens-MacBook-Air.local>
2025-02-12 22:09:28 +0530

    created calculator program

---
+++ src/main/java/org/example/practice/Calculator.java
@@ -0,0 +1,14 @@
+package org.example.practice;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first number");
+        int a = sc.nextInt();
+        System.out.println("enter second number");
+        int b = sc.nextInt();
+        System.out.println("sum of num1: "+a +"and num2: "+b+ " is : "+(a+b));
+    }
+}
